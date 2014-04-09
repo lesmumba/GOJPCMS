54cad2a83804e895a139673fa900452a55c23263
Oshane Bailey <b4.oshany@gmail.com>
2014-04-09 01:37:04 -0500

    delete unecessary files and adjust the map viewer

--- gojpcms/MapComponent.java
+++ gojpcms/MapComponent.java
@@ -3,8 +3,9 @@ import gojpcms.map.Map;
 
 import javax.swing.*;
 public class MapComponent {
-	 public static void main(String [] args)
-	    {
+     public static void main(String [] args)
+        {
+	        
 		 	 Map gmap = new Map("Kingston", 17.9833, -76.800);
 	         JOptionPane.showMessageDialog(null,new JLabel(gmap.getMap()));
 	    }
